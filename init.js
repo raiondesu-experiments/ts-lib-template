cbb203a9a3a18eaf2a464d85cf4b9acb1993b54b
Raiondesu <a.raiondesu@gmail.com>
2019-11-11 23:10:38 +0300

    Feature [proper process exit] init

--- init.js
+++ init.js
@@ -26,16 +26,18 @@ try {
   fs.unlinkSync('README.md');
   fs.renameSync('README.template.md', 'README.md');
 
-  exec('npm un -D replace-in-file yargs')
+  exec('npm un -D replace-in-file yargs').on('exit', (code) => {
+    process.exit(code);
+  });
 
   fs.unlinkSync('init.js');
 
-  return 0;
+  return process.exit(0);
 } catch (e) {
   console.error(`Please, make sure you have provided the package information in the following format:
   node init {github-username-or-org}/{package-name}`);
 
   console.error(e);
 
-  return 1;
+  return process.exit(1);
 }
