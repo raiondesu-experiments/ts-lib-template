01e486f5b2e7eaca98c6e29222c084889cedcd02
Raiondesu <a.raiondesu@gmail.com>
2019-11-11 23:14:47 +0300

    Fix [parallelization] init

--- init.js
+++ init.js
@@ -21,18 +21,18 @@ try {
     files: process.cwd() + '/**',
     from: /{{owner}}/g,
     to: packageOrg
-  }));
+  })).then(() => {
+    fs.unlinkSync('README.md');
+    fs.renameSync('README.template.md', 'README.md');
 
-  fs.unlinkSync('README.md');
-  fs.renameSync('README.template.md', 'README.md');
+    exec('npm un -D replace-in-file yargs').on('exit', (code) => {
+      process.exit(code);
+    });
 
-  exec('npm un -D replace-in-file yargs').on('exit', (code) => {
-    process.exit(code);
+    fs.unlinkSync('init.js');
+
+    process.exit(0);
   });
-
-  fs.unlinkSync('init.js');
-
-  return process.exit(0);
 } catch (e) {
   console.error(`Please, make sure you have provided the package information in the following format:
   node init {github-username-or-org}/{package-name}`);
